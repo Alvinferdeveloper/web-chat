3047fcecc3b3cdc8f582088b2b8b4235211f6441
Albin <alvinfer67@gmail.com>
2025-03-18 23:14:51 -0600

    feat: protecting route /

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import withAuth from 'next-auth/middleware'
+export { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+    signOut: "/login",
+  },
+})
+ 
+export const config = {
+  matcher: [
+    '/',
+  ],
+}
