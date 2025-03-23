2e138b290c5f9de2dc0c80b74fc4c1edb15f9a05
Albin <alvinfer67@gmail.com>
2025-03-22 20:44:59 -0600

    feat: register user

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from 'next-auth/providers/github'
+import GithubProvider from 'next-auth/providers/github';
+import supabase from "@/lib/supabase";
+import { randomUUID } from "node:crypto";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -14,14 +16,33 @@ export const authOptions: NextAuthOptions = {
     })
   ],
   callbacks: {
-    async signIn({ user, account, profile }) {
+    async signIn({ user, account }) {
       if (account?.provider === 'google' || account?.provider === 'github') {
-        return true
+        const { data: existingUser } = await supabase
+          .from('user')
+          .select()
+          .eq('provider_id', user.id)
+          .eq('provider', account.provider.toUpperCase())
+          .single();
+
+        if (!existingUser) {
+          const { error } = await supabase.from('user').insert([
+            {
+              ...user,
+              id: randomUUID(),
+              provider: account.provider.toUpperCase(),
+              provider_id: user.id
+            }
+          ]);
+          return !error;
+        }
+
+        return true;
       }
-      return false
+      return false;
     }
   }
 }
 export const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST}; 
+export { handler as GET, handler as POST }; 
