5b7525ec011996b8e55ebf94ea48b3062882b21e
Albin <alvinfer67@gmail.com>
2025-03-23 23:37:34 -0600

    add: custom drop down menu component for profile options

---
+++ src/app/components/customDropDownMenu.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { LogOut } from "lucide-react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+
+interface DropdownMenuProps {
+  trigger: React.ReactNode
+}
+
+export default function CustomDropdownMenu({ trigger }: DropdownMenuProps) {
+  return (
+    <div className="w-full max-w-md p-6">
+      <div className="flex justify-end">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            {trigger}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="w-56 bg-gray-900 text-gray-100 border-gray-800">
+            <DropdownMenuItem
+              className="flex cursor-pointer items-center py-3  data-[highlighted]:hover:bg-gray-800 data-[highlighted]:hover:text-white"
+            >
+              <LogOut className="mr-3 h-4 w-4" />
+              <span>Log out</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  )
+}
+
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,18 +3,21 @@ import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
 import { useSession } from "next-auth/react";
 import { User2 } from "lucide-react";
 import { orbitron } from "../font";
+import CustomDropdownMenu from "./customDropDownMenu";
 
 export default function Header() {
     const { data: session } = useSession();
     return (
         <header className="w-full h-10 left-0 fixed flex justify-between px-7 y- items-center">
             <p className={`${orbitron.className} text-2xl text-white`}>Mi logo </p>
-            <Avatar>
-                <AvatarImage src={session?.user?.image || ''} />
-                <AvatarFallback className="bg-gray-200">
-                    <User2 className="h-5 w-5 text-gray-600" />
-                </AvatarFallback>
-            </Avatar>
+            <CustomDropdownMenu trigger={
+                <Avatar className="cursor-pointer hover:opacity-80">
+                    <AvatarImage src={session?.user?.image || ''} />
+                    <AvatarFallback className="bg-gray-200">
+                        <User2 className="h-5 w-5 text-gray-600" />
+                    </AvatarFallback>
+                </Avatar>
+            } />
         </header>
     )
 }
