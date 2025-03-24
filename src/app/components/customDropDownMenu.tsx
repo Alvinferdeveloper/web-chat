1cb25de957116b6b2a8ba35d898b1e5b19ccd63c
Albin <alvinfer67@gmail.com>
2025-03-23 23:41:52 -0600

    feat: sign out

--- src/app/components/customDropDownMenu.tsx
+++ src/app/components/customDropDownMenu.tsx
@@ -1,12 +1,17 @@
 "use client"
 import { LogOut } from "lucide-react"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { signOut } from "next-auth/react";
 
 interface DropdownMenuProps {
   trigger: React.ReactNode
 }
 
 export default function CustomDropdownMenu({ trigger }: DropdownMenuProps) {
+  const handleLogout = async () => {
+    await signOut();
+  }
+
   return (
     <div className="w-full max-w-md p-6">
       <div className="flex justify-end">
@@ -16,6 +21,7 @@ export default function CustomDropdownMenu({ trigger }: DropdownMenuProps) {
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end" className="w-56 bg-gray-900 text-gray-100 border-gray-800">
             <DropdownMenuItem
+              onClick={handleLogout}
               className="flex cursor-pointer items-center py-3  data-[highlighted]:hover:bg-gray-800 data-[highlighted]:hover:text-white"
             >
               <LogOut className="mr-3 h-4 w-4" />
