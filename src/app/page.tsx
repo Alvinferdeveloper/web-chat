bdb45175a46115f5c3e44e5c8d707d8a5a702aeb
Albin <alvinfer67@gmail.com>
2025-03-23 22:57:48 -0600

    add: Header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Card, CardContent } from "@/components/ui/card"
 import LlmProcessing from "@/app/components/llmProcessing"
+import Header from "./components/header"
 
 export default async function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-950 to-slate-800 p-8">
+      <Header />
       <Card className="max-w-4xl mx-auto bg-inherit border-none ">
         <CardContent className="space-y-6">
           <LlmProcessing />
