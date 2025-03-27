5c76969ed923311fa6a884de92d17051e7e3695b
Albin <alvinfer67@gmail.com>
2025-03-26 20:36:06 -0600

    fix: google's profile picture is not displayed

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -7,12 +7,13 @@ import CustomDropdownMenu from "./customDropDownMenu";
 
 export default function Header() {
     const { data: session } = useSession();
+    console.log(session?.user?.image)
     return (
         <header className="w-full h-10 left-0 fixed flex justify-between px-7 y- items-center">
             <p className={`${orbitron.className} text-2xl text-white`}>Mi logo </p>
             <CustomDropdownMenu trigger={
                 <Avatar className="cursor-pointer hover:opacity-80">
-                    <AvatarImage src={session?.user?.image || ''} />
+                    { session?.user?.image && <AvatarImage src={session?.user?.image} />}
                     <AvatarFallback className="bg-gray-200">
                         <User2 className="h-5 w-5 text-gray-600" />
                     </AvatarFallback>
