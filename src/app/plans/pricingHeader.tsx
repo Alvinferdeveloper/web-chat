96bf81cc65fd0e38ee42d3e3e5f4283e445c2945
Albin <alvinfer67@gmail.com>
2025-03-27 23:25:51 -0600

    feat: Button to navigate back from the pricing window

--- src/app/plans/pricingHeader.tsx
+++ src/app/plans/pricingHeader.tsx
@@ -1,10 +1,18 @@
+"use client"
+import { X } from "lucide-react"
+import { useRouter } from "next/navigation"
 export default function PricingHeader() {
+    const router = useRouter()
+    const handleCloseWindow = () => {
+        router.back();
+    }
     return (
         <div className="text-center mt h-[20%] mb-4 flex justify-center flex-col">
             <h1 className="text-3xl font-bold tracking-tight sm:text-4xl md:text-5xl text-white">Planes simples y transparentes</h1>
-            <p className="mt-4 text-lg text-muted-foreground max-w-3xl mx-auto">
+            <p className="mt-4 text-lg text-muted-foreground max-w-3xl mx-auto relative top-0 right-0">
                 Elija el plan perfecto para su negocio. Al crear tu cuenta obtienes el plan gratuito.
             </p>
+            <X className="absolute top-8 right-8 cursor-pointer text-muted-foreground hover:text-white" onClick={handleCloseWindow} />
         </div>
     )
 }
