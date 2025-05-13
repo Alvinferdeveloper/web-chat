4ab9ceef53e37df64a341b7bef9bc19827f1dd8c
Albin <alvinfer67@gmail.com>
2025-05-12 22:29:39 -0600

    fix: unmatch filed error in suscription field in the usage_log table

--- src/app/api/services/usage.service.ts
+++ src/app/api/services/usage.service.ts
@@ -1,7 +1,7 @@
 import supabase from "@/lib/supabase";
 export class UsageService {
     static async isFreePlanLimitReached(subscriptionId: string, planId: number) {
-        const { data: usage } = await supabase.from('usage_log').select('tokens_used').eq('subscription_id', subscriptionId).single();
+        const { data: usage } = await supabase.from('usage_log').select('tokens_used').eq('suscription_id', subscriptionId).single();
         const { data: plan } = await supabase.from('plan').select('name, tokens').eq('id', planId).single();
         if(plan?.name == 'FREE' && usage?.tokens_used >= plan?.tokens){
             return true;
