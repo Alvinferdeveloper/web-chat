bd9cee858d32609844a2ac5725440e80ccb6d844
Albin <alvinfer67@gmail.com>
2025-06-30 21:39:26 -0600

    delete: unused api route providers

--- src/app/api/providers/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { availableProviders } from "@/app/api/data/providers";
-
-export async function GET(){
-    const providers = availableProviders.map(({ createProvider, ...rest }) => rest);
-    return NextResponse.json(providers);
-}
